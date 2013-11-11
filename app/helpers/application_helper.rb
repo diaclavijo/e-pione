7bee5376e0021f1deab0f0dfba4f9ccd5a74ea24
yarilo <error500.dos@gmail.com>
2013-11-11 18:29:01 +0000

    removing ide settings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,25 @@ module ApplicationHelper
   class MyFormBuilder < ActionView::Helpers::FormBuilder
 
     def text_field(method, options = {})
-      super(method, options)
+      options[:class] ='form-control'
+      @template.content_tag(
+          :div,
+          super(method,options),
+          class: 'col-md-4'
+      )
+
     end
+
+    def number_field(method, options = {})
+      options[:class] ='form-control'
+      @template.content_tag(
+        :div,
+        super(method,options),
+        class: 'col-md-4'
+      )
+
+    end
+
     def select(method, choices, options = {}, html_options = {})
       html_options[:class] = 'form-control'
       @template.content_tag(
@@ -30,7 +47,15 @@ module ApplicationHelper
           super(value, options, &block),
           class: 'col-md-offset-4 col-md-10'
       )
+    end
 
+    def submit(value=nil, options={})
+      options[:class] = 'btn btn-default'
+      @template.content_tag(
+          :div,
+          super(value, options),
+          class: 'col-md-offset-4 col-md-10'
+          )
     end
 
   end
