89bd62865fca79ebda00f5adbbb910222dca8a49
diaclavijo <david@diazclavijo.com>
2014-02-10 18:44:00 +0000

    removed migration,  not needed

--- db/migrate/20140207130109_change_date_from_consultations.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDateFromConsultations < ActiveRecord::Migration
-  def change
-    remove_column :consultations, :date, :date
-    add_column    :consultations, :date, :datetime
-  end
-end
