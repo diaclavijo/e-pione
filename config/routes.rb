e0e8d887acc2eb410828d2774484f964fd6f777a
diaclavijo <david@diazclavijo.com>
2014-02-14 13:15:08 +0000

    removed unnnecesary routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,11 @@
 RailsSIAD::Application.routes.draw do
   resources :consultations, only: [] do
-    get 'cognitive_symptomatology', to: 'cognitive_symptomatologies#new_or_edit', as: 'cognitive_symptomatology'
-    post 'cognitive_symptomatology', to: 'cognitive_symptomatologies#create', as: nil
-    patch 'cognitive_symptomatology', to: 'cognitive_symptomatologies#update', as: nil
-
     resources :test_minimentals, only: [:new, :create, :show, :destroy]
-    resources :test_iqcodes, only: [:new, :create, :show, :destroy]
     resources :test_mecs, only: [:new, :create, :show, :destroy]
     resources :test_pfeiffers, only: [:new, :create, :show, :destroy]
     resources :test_relojs, only: [:new, :create, :show, :destroy]
-
-    get 'no_cognitive_symptomatology', to: 'no_cognitive_symptomatologies#new_or_edit', as: 'no_cognitive_symptomatology'
-    post 'no_cognitive_symptomatology', to: 'no_cognitive_symptomatologies#create', as: nil
-    patch 'no_cognitive_symptomatology', to: 'no_cognitive_symptomatologies#update', as: nil
-
-    resources :test_yesavage4s, only: [:new, :create, :show, :destroy]
-    resources :test_yesavage10s, only: [:new, :create, :show, :destroy]
-    resources :test_yesavage15s, only: [:new, :create, :show, :destroy]
-    resources :test_yesavage30s, only: [:new, :create, :show, :destroy]
-    resources :test_npis, only: [:new, :create, :show, :destroy]
-
-    get 'exploracion_funcional', to: 'exploracion_funcional#show', as: 'exploracion_funcional'
-    resources :test_barthels, only: [:new, :create, :show, :destroy]
-    resources :test_fasts, only: [:new, :create, :show, :destroy]
-    resources :test_katzs, only: [:new, :create, :show, :destroy]
-    resources :test_lawtons, only: [:new, :create, :show, :destroy]
-
-    resources :habitos, only: [:index, :create, :destroy]
-    resources :pathologies, only: [:index, :create, :destroy]
-    resources :pruebas, only: [:index, :create, :destroy]
-
-    get 'neurological_examination', to: 'neurological_examinations#new_or_edit', as: 'neurological_examination'
-    post 'neurological_examination', to: 'neurological_examinations#create', as: nil
-    patch 'neurological_examination', to: 'neurological_examinations#update', as: nil
-
-    get 'physical_examination', to: 'physical_examinations#new_or_edit', as: 'physical_examination'
-    post 'physical_examination', to: 'physical_examinations#create', as: nil
-    patch 'physical_examination', to: 'physical_examinations#update', as: nil
-
   end
+
   get 'diagnosis', to: 'diagnoses#diagnosis', as: 'diagnosis'
 
   resources :patients  do
@@ -57,8 +24,6 @@ RailsSIAD::Application.routes.draw do
   end
 
   devise_for :physicians, controllers: {:sessions => "physicians/sessions"}
-
-
 
   devise_for :admins  # This line has to be above mount RailsAdmin::Engine , if not , it makes an endless loop.
 
