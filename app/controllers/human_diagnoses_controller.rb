f01f9db8c33d2e93c625a8b27be41ae2c46d5b05
diaclavijo <david@diazclavijo.com>
2014-03-26 18:44:12 +0000

    correct storing

--- app/controllers/human_diagnoses_controller.rb
+++ app/controllers/human_diagnoses_controller.rb
@@ -2,18 +2,18 @@ class HumanDiagnosesController < ConsultationResourcesController
 
 
   def new
-    validate_tests
+		@test_reloj = @consultation.test_relojs.last
+		@computer_diagnosis = @consultation.computer_diagnoses.last
     @human_diagnosis = @consultation.human_diagnoses.build
-    @computer_diagnosis = @consultation.computer_diagnoses.last
   end
 
   # GET /diagnoses
   # GET /diagnoses.json
   def create
-    validate_tests
+		@test_reloj = @consultation.test_relojs.last
     @computer_diagnosis = @consultation.computer_diagnoses.last
     @human_diagnosis = @consultation.human_diagnoses.build human_diagnosis_params
-
+		@human_diagnosis.physician = current_physician
     respond_to do |format|
       if @human_diagnosis.save
         format.html { redirect_to [@consultation.patient, @consultation], notice: t('errors.human_diagnosis.notice.created') }
@@ -29,10 +29,5 @@ class HumanDiagnosesController < ConsultationResourcesController
     def human_diagnosis_params
       params.require(:human_diagnosis).permit(:diagnosis)
     end
-
-  def validate_tests
-    @test_reloj = @consultation.test_relojs.first
-  end
-
 
 end
