f0674819caabdce397f9c4383fa4a409e004fdda
Yarilo <error500.dos@gmail.com>
2014-04-02 17:39:18 +0100

    Erased all the views to implement MVP (Quick diagnosis only). Also adapted font awesome to show itself in ie-7

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,5 +1,5 @@
 crumb :root do
-  link 'Inicio', root_path
+  link 'Diagnóstico rápido', new_quick_diagnosis_path
 end
 
 crumb :patients do
@@ -9,44 +9,11 @@ end
 crumb :contact do
   link 'Contactar', new_contacts_path
 end
-crumb :quick_diagnosis do
-  link 'Diagnóstico Rápido', new_quick_diagnosis_path
-end
 
 crumb :correct_quick_diagnosis do
   link 'Resultado diagnóstico', new_quick_diagnosis_path
-  parent :quick_diagnosis
+  parent :root
 end
-crumb :new_patient do
-  link 'Registrar nuevo paciente', new_patient_path
-  parent :patients
-end
-
-crumb :edit_patient do |patient|
-  link 'Editar paciente '+patient.full_name, edit_patient_path(patient)
-  parent :patients
-end
-
-crumb :patient do |patient|
-  link patient.full_name, patient_consultations_path(patient)
-  parent :patients
-end
-
-crumb :patient_full_history do |patient|
-  link 'Historia completa del paciente', patient_full_history_path(patient)
-  parent :patient, patient
-end
-
-crumb :new_patient_consultation do |patient|
-  link 'Nueva consulta'
-  parent :patient, patient
-end
-
-crumb :patient_consultation do |patient, consultation|
-  link ('Consulta '+consultation.date.to_s), patient_consultation_path(patient, consultation)
-  parent :patient, patient
-end
-
 
 
 
@@ -70,7 +37,7 @@ tests.each{|test_key|
 
 	crumb :"new_quick_#{test_key}" do |consultation|
 		link 'Nuevo '+test_key.classify.constantize::NAME
-		parent :quick_diagnosis
+		parent :root
 	end
 }
 
