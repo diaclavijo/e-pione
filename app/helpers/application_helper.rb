f873d75d6333d44e076b2c8ae0a609a623fec939
Yarilo <error500.dos@gmail.com>
2014-05-27 16:24:57 +0100

    Updated selects styling in new view and minor changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -175,12 +175,16 @@ module ApplicationHelper
 
   def educations
     {
-        'Analfabeto             (0 años)'  => 0 ,
-        'Estudios mínimos        (6 años)'  => 6 ,
-        'Estudios Primarios      (10 años)' => 10 ,
-        'Estudios Secundarios    (12 años)' => 12,
-        'Estudios Universitarios (16 años)' => 16,
-        'Otro (introducir número de años)'  => -1
+        'Inferior a Estudios Primarios' => 0,
+        'Estudios Primarios           ' => 1 ,
+        'Superior a Estudios Primarios' => 2,
+    }
+  end
+
+  def genders
+    {
+        'Hombre'  => 0,
+        'Mujer'   => 1
     }
   end
 
